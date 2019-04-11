f80dfd879e297cc5e0be168aa92f76bb78f86984
Hanvitha Gavini <gavini.honey@gmail.com>
2019-04-11 16:15:28 -0500

    test

--- src/main/java/org/acme/quarkus/sample/HelloResource.java
+++ src/main/java/org/acme/quarkus/sample/HelloResource.java
@@ -7,9 +7,10 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class HelloResource {
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hola!";
+        return "Praise God!";
     }
 }
